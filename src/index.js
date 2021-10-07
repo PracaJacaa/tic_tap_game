20f79407c98087ed74d7e9b52a059c060a324d89
Jacek Gipsiak <gipsiak77@gmail.com>
2021-10-07 20:25:50 +0000

    mamy to

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 
-
-class Game extends React.Component{
+class Square extends React.Component{
     constructor(props){
         super(props);
     }
     render(){
         return(
-        <div className="Game">
-            <div className="Board">
-                <Board  value={...} onClick={...} />
-            </div>
-        </div>
+            <button  
+            onClick={() => this.props.handleboxClick()}>
+                {this.props.value}
+            </button>     //later add some onchange 
         );
     }
 }
@@ -21,43 +19,69 @@ class Board extends React.Component{
     constructor(props){
         super(props);
     }
+
     render(){
-        const emptyboxes = [0,3,6]
-        const boxes = Array(3).fill(null);
-        return boxes.map((square,usless)=>{
+        const RowBoxes = [0,3,6];
+        const RowArry = Array(3).fill(null);
+
+        return RowBoxes.map((RowBoxe, emptyindex) =>{
             return(
-                <div className="TheBoard" >
-                    {boxes.map((emptyboxes, alsousless)=>{
-                        let boxindex = emptyboxes + alsousless;
+                <div className="rowindex">
+                    {RowArry.map((RowArr,anotherEmptyIndex)=>{
+                        let RowArrIndex = RowBoxe + anotherEmptyIndex;
                         return(
                             <Square 
-                                value="X" //this.props.boxes[index]
-                                boxClick={() => { //this.props.
-                                console.log("blabla");
-                                }}
+                            value={this.props.RowArry[RowArrIndex]}
+                            handleboxClick={() => {
+                                this.props.handleboxClick(RowArrIndex);
+                            }}
                             />
-                        )
-
+                        );
                     })}
-                    
                 </div>
-            );
-            
-        })
+                );
+        });
     }
 }
 
-
-class Square extends React.Component{
+class Game extends React.Component{
     constructor(props){
         super(props);
+        this.state = { 
+            RowArry: Array(9).fill(null),
+            nextSymbol: "X",
+        };
+    this.handleboxClick = this.handleboxClick.bind(this);
     }
 
-    render() {
-        return <button>{this.props.value}</button>
+    handleboxClick(index){
+        console.log(`User clicked ${index}`);
+        const RowArry = this.state.RowArry;
+
+        if (RowArry[index] !== null){
+            return;
+        }
+
+        RowArry[index]= this.state.nextSymbol;
+
+        this.setState({
+            RowArry: RowArry,
+            nextSymbol: this.state.nextSymbol === "X" ? "O" : "X",
+        });
+
+    }
+    render(){
+        return(
+        <div className="game">
+            <div className="board">
+                <Board   
+                handleboxClick={this.handleboxClick}
+                RowArry={this.state.RowArry}
+                />
+            </div>
+        </div>
+        );
     }
 }
 
-
-
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<Game/>, document.getElementById("root"));
