6ea8b8d123b63056e5aa1490f6845be9a40380b5
Jacek Gipsiak <gipsiak77@gmail.com>
2021-10-11 09:04:24 +0000

    helolo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+//https://www.bing.com/search?q=function+calculateWinner(squares)+%7B+const+lines+%3D+%5B+%5B0%2C+1%2C+2%5D%2C+%5B3%2C+4%2C+5%5D%2C+%5B6%2C+7%2C+8%5D%2C+%5B0%2C+3%2C+6%5D%2C+%5B1%2C+4%2C+7%5D%2C+%5B2%2C+5%2C+8%5D%2C+%5B0%2C+4%2C+8%5D%2C+%5B2%2C+4%2C+6%5D%2C+%5D%3B+for+(let+i+%3D+0%3B+i+%3C+lines.length%3B+i%2B%2B)+%7B+const+%5Ba%2C+b%2C+c%5D+%3D+lines%5Bi%5D%3B+if+(squares%5Ba%5D+%26%26+squares%5Ba%5D+%3D%3D%3D+squares%5Bb%5D+%26%26+squares%5Ba%5D+%3D%3D%3D+squares%5Bc%5D)+%7B+return+squares%5Ba%5D%3B+%7D+%7D+return+null%3B+%7D&cvid=df1672d60d6b4ec69a6cf74919644157&aqs=edge..69i57.681j0j1&FORM=ANNTA1&PC=U531
+// https://github.com/itsrave/tictactoe-react/blob/01f696da87a86bf38befb571a81b0a8781b73568/src/index.js#L182
 class Square extends React.Component{
     constructor(props){
         super(props);
@@ -44,9 +45,9 @@ class Board extends React.Component{
         const RowBoxes = [0,3,6];
         const RowArry = Array(3).fill(null);
         const winner = this.Whowins(RowArry);
-        let RowArrIndex;
+        let status;
         if(winner){
-            RowArrIndex = `Wygrywa ${winner}`;
+            status = `Wygrywa ${winner}`;
         }else{
             
         }
